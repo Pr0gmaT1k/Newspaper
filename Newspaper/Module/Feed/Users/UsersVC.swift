b8bd1ae043003a3ceb6cafdd1902ad61bff531b3
azerty <julien.cloupet@gmail.com>
2019-10-06 13:32:21 -0300

    missing files

--- Newspaper/Module/Feed/Users/UsersVC.swift
+++ Newspaper/Module/Feed/Users/UsersVC.swift
@@ -9,19 +9,54 @@
 import UIKit
 
 // MARK:- Delegate
-protocol FriendVCDelegate: class {
-    
+protocol UsersVCDelegate: class {
+    func requestUser(vc: UsersVC)
 }
 
 // MARK:- Class
-final class FriendVC: UIViewController {
+final class UsersVC: UIViewController {
     // MARK:- Properties
-    weak var delegate: FriendVCDelegate?
+    weak var delegate: UsersVCDelegate?
+    private var source: [User]?
+    private let colorSource: [Color] = [ColorName.primary.color,
+                                        ColorName.secondary.color,
+                                        ColorName.scarlett.color]
     
     // MARK:- IBOutlets
+    @IBOutlet private weak var tableView: UITableView!
     
     // MARK:- Funcs
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.register(cellType: UserTVCell.self)
+        delegate?.requestUser(vc: self)
+    }
+    
+    func updateUser(users: Users?) {
+        self.source = users?.users.reversed()
+        self.tableView.reloadData()
+    }
+    
+    /// Circular array applied to self.colorSource.
+    private func getColors(index: Int) -> Color {
+        if self.colorSource.isEmpty { return ColorName.primary.color }
+        let mod = index % self.colorSource.count
+        let offset = index >= 0 ? 0 : self.colorSource.count
+        let idx = mod == 0 ? 0 : mod + offset
+        return self.colorSource[idx]
+    }
+}
+
+// MARK:- UITableView DataSource
+extension UsersVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(for: indexPath) as UserTVCell
+        cell.fill(user: source?[indexPath.row], color: getColors(index: indexPath.row))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return source?.count ?? 0
     }
 }
